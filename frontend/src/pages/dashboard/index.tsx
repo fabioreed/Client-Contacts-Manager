64d05996a7a43b20c80d53672f7f70836f14139a
Fabio Luiz <fabioeeereed@gmail.com>
2023-08-04 14:52:56 -0300

    fix some console.logs

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,9 +1,7 @@
-import { useContext, useEffect } from "react"
+import { useContext } from "react"
 import Header from "../../components/Header"
 import { ButtonDash, Card, ContainerOfTheButtons, ListCard, MainContainer, SectionContainerDash } from "./style"
 import { DashContext } from "../../providers/DashContext"
-import { api } from "../../services/api"
-import { useNavigate } from "react-router-dom"
 import ModalCreate from "../../components/ModalCreate"
 import ModalEdit from "../../components/ModalEdit"
 import Empty from "../../components/Empty"
@@ -12,35 +10,7 @@ import { Badge } from '@mui/material'
 import EditProfileForm from "../../components/EditProfile"
 
 const Dashboard = () => {
-  const navigate = useNavigate()
-  const { contact, modal, setModal, removeContact, listAllContacts, setListAllContacts, profileEditModal, selectedContact, setSelectedContact, profile, setProfile } = useContext(DashContext)
-  // const [profile, setProfile] = useState<IClient[]>([])
-
-  // useEffect(() => {
-  //   const token = localStorage.getItem('@clientToken')
-  //   if (token) {
-  //     const getContacts = async () => {
-  //       try {
-  //         const res = await api.get('/contacts', {
-  //           headers: {
-  //             Authorization: `Bearer ${token}`
-  //           }
-  //         })
-
-  //         setProfile(res.data)
-
-  //         navigate('/dashboard')
-  //       } catch (error) {
-  //         console.log(error)
-  //         console.log('Erro no useEffect!')
-          
-  //         navigate('/')
-  //         localStorage.clear()
-  //       }
-  //     }
-  //     getContacts()
-  //   }
-  // }, [contact])
+  const { modal, setModal, removeContact, listAllContacts, setListAllContacts, profileEditModal, selectedContact, setSelectedContact, profile } = useContext(DashContext)
 
   return (
     <>
